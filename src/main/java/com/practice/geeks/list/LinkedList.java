a60c907aa6e81858e4df0a365f2b5d36b4bf9195
Developer-Y <developer.yp@gmail.com>
2016-10-16 18:45:33 +0530

    Basic LL and Tree classes

---
+++ src/main/java/com/practice/geeks/list/LinkedList.java
@@ -0,0 +1,14 @@
+package com.practice.geeks.list;
+
+public class LinkedList {
+	
+	Node head;
+	
+	public LinkedList(){
+		
+	}
+	
+	public LinkedList(int headVal){
+		this.head = new Node(headVal);
+	}
+}
---
+++ src/main/java/com/practice/geeks/tree/BinaryTree.java
@@ -0,0 +1,13 @@
+package com.practice.geeks.tree;
+
+public class BinaryTree {
+	
+	Node root;
+	
+	public BinaryTree(){
+	}
+	
+	public BinaryTree(int rootVal){
+		this.root = new Node(rootVal);
+	}
+}
